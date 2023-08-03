7fea1ca14615e77058b968466e8c0beec59c1976
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2023-08-03 20:07:36 +0000

    Deploying to gh-pages from @ OlehKozubJS/goit-react-hw-05-movies@6b550bff916ff8cf43fb5051554581237a1c639e 🚀

--- static/js/221.96ede7f5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[221],{221:function(e,t,r){r.r(t),r.d(t,{Movies:function(){return u}});var n=r(689),c=r(184),u=function(){return(0,c.jsx)("div",{children:(0,c.jsx)(n.j3,{})})}}}]);
-//# sourceMappingURL=221.96ede7f5.chunk.js.map
---
+++ static/js/221.f5963d1f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[221],{221:function(e,n,r){r.r(n),r.d(n,{Movies:function(){return c}});var t=r(689),s=r(791),a=r(184),c=function(){return(0,a.jsx)("div",{children:(0,a.jsx)(s.Suspense,{fallback:(0,a.jsx)("div",{children:"Loading subpage..."}),children:(0,a.jsx)(t.j3,{})})})};n.default=c}}]);
+//# sourceMappingURL=221.f5963d1f.chunk.js.map
