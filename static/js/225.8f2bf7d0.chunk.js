ecd06c2fdad4817ea5da396de83a9dacbdb81036
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2023-08-03 20:49:52 +0000

    Deploying to gh-pages from @ OlehKozubJS/goit-react-hw-05-movies@064f4c9d177b39fca55e75bdf90bc0b63d909364 🚀

--- static/js/225.8f2bf7d0.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[225],{225:function(){}}]);
---
+++ static/js/597.448842ea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[597],{597:function(e,n,t){t.r(n),t.d(n,{Cast:function(){return c}});var r=t(791),a=t(689),s=t(184),c=function(){return(0,s.jsx)("div",{children:(0,s.jsx)(r.Suspense,{fallback:(0,s.jsx)("div",{children:"Loading subpage..."}),children:(0,s.jsx)(a.j3,{})})})};n.default=c}}]);
+//# sourceMappingURL=597.448842ea.chunk.js.map
