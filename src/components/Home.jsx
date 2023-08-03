d6b3882ebbe6f3f4d06104d2163b9d5edb49ad8f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 00:59:14 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export const Home = () => {
-  return (
-    <div>
-      <Suspense fallback={<div>Loading subpage...</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
