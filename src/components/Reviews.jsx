f62ab5fa237251e7241dae69d9a088ceaacc9067
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 01:02:48 +0300

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,13 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export const Reviews = () => {
-  return (
-    <div>
-      <Suspense fallback={<div>Loading subpage...</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Reviews;
