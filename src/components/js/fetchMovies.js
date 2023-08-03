064f4c9d177b39fca55e75bdf90bc0b63d909364
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-03 23:47:40 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -1,1 +1,17 @@
 // https://api.themoviedb.org/3/movie/550?api_key=e5eae3e86944a38d68a9b48044e5baf0
+import axios from 'axios';
+
+export async function fetchImages(searchResult, pageNumber) {
+  const searchParams = new URLSearchParams({
+    key: '37447910-ed3fb6b843fd00e4ff71a16f5',
+    q: searchResult,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: pageNumber,
+    per_page: 12,
+  });
+
+  const response = await axios.get('https://pixabay.com/api/?' + searchParams);
+  return await response.data;
+}
