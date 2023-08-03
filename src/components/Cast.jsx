367f75fd08065b71eb58e46f0eeeecbe22cc1e99
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-03 23:07:29 +0300

    Update Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 export const Cast = () => {
   return (
     <div>
-      <Outlet />
+      <Suspense fallback={<div>Loading subpage...</div>}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
+
+export default Cast;
