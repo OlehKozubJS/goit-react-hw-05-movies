2edc92bcd709da47e6c4def0902208ceebc1d0ba
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 00:27:26 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -6,9 +6,10 @@
 // /movies/get-movie-reviews
 import axios from 'axios';
 
-export async function fetchImages(searchResult, pageNumber) {
+export async function fetchMovies(searchResult, pageNumber) {
   const response = await axios.get(
     'https://api.themoviedb.org/3/movie/550?api_key=e5eae3e86944a38d68a9b48044e5baf0'
   );
+  console.log(response);
   return await response.data;
 }
