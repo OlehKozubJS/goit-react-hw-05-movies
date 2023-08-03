cf7ed4967627d0a482cab66b595dcfdbaadd2c35
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-03 23:48:15 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export async function fetchImages(searchResult, pageNumber) {
   const searchParams = new URLSearchParams({
-    key: '37447910-ed3fb6b843fd00e4ff71a16f5',
+    key: 'e5eae3e86944a38d68a9b48044e5baf0',
     q: searchResult,
     image_type: 'photo',
     orientation: 'horizontal',
