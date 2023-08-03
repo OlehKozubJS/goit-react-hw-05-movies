735e4183870892020c5028e807e8464f585449f7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 00:25:59 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -7,18 +7,8 @@
 import axios from 'axios';
 
 export async function fetchImages(searchResult, pageNumber) {
-  const searchParams = new URLSearchParams({
-    key: 'e5eae3e86944a38d68a9b48044e5baf0',
-    q: searchResult,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: pageNumber,
-    per_page: 12,
-  });
-
   const response = await axios.get(
-    'https://api.themoviedb.org/3/movie/550?' + searchParams
+    'https://api.themoviedb.org/3/movie/550?api_key=e5eae3e86944a38d68a9b48044e5baf0'
   );
   return await response.data;
 }
