56c2e8a6ee5cb3c79117611dbf065b00d3cc6acf
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 21:02:55 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { fetchFavouriteMovies } from './js/fetchMovies';
 
 export const Home = () => {
+  const favouriteMovies = fetchFavouriteMovies();
+
   return (
     <div>
       <h1>Trending Today</h1>
-      <ul></ul>
+      <ul>{}</ul>
     </div>
   );
 };
