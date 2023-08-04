4c231741a4d995abcb54c8df7de60909b1521f7b
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-05 00:22:55 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -22,7 +22,7 @@ export const MovieDetails = () => {
 
   return (
     <div>
-      <Link to={backLinkHref}></Link>
+      <Link to={backLinkHref}>Back to movies</Link>
       <h2></h2>
       <p></p>
       <h3>Overview</h3>
