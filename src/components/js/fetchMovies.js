57c4a216276a7661797a9e97b2141cf5dc6c1b1e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 22:04:46 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 
 const API_KEY = 'e5eae3e86944a38d68a9b48044e5baf0';
+const URL = 'https://api.themoviedb.org/3/';
 
 export const fetchFavouriteMovies = async () => {
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
     time_window: 'day',
   });
-  const response = await axios.get(
-    'https://api.themoviedb.org/3/trending/all/day?' + searchParams
-  );
+  const response = await axios.get(URL + 'trending/all/day?' + searchParams);
+  return await response.data;
+};
+
+export const fetchMovieByName = async name => {
+  const searchParams = new URLSearchParams({
+    api_key: API_KEY,
+    query: name,
+  });
+
+  const response = await axios.get(URL + 'search/movie?' + searchParams);
   return await response.data;
 };
