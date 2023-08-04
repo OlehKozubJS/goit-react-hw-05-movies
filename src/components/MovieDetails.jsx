05ce79dcb01f327a578ced606f6dc461c2006edd
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 23:43:57 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,7 +1,20 @@
+import { fetchMovieDetails } from './js/fetchMovies';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export const MovieDetails = () => {
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      try {
+        const result = await fetchMovieDetails(searchQuery);
+        setMovies(result.results);
+      } catch {
+        console.log('The fetch has failed');
+      }
+    };
+    getMovieDetails();
+  }, [searchQuery]);
+
   return (
     <div>
       <Suspense fallback={<div>Loading subpage...</div>}>
