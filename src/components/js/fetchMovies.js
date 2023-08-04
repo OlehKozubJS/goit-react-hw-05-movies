3d84b7325a46f56681a5d5d4b68a5862197ea223
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 20:32:56 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -1,15 +1,11 @@
-// https://api.themoviedb.org/3/movie/550?api_key=e5eae3e86944a38d68a9b48044e5baf0
-// /trending/get-trending
-// /search/search-movies
-// /movies/get-movie-details
-// /movies/get-movie-credits
-// /movies/get-movie-reviews
 import axios from 'axios';
 
 export async function fetchMovies() {
   const response = await axios.get(
-    'https://api.themoviedb.org/3/trending/all/day?api_key=e5eae3e86944a38d68a9b48044e5baf0'
+    'https://api.themoviedb.org/3/search/movie?api_key=e5eae3e86944a38d68a9b48044e5baf0&include_adult=false&language=en-US&page=1'
   );
   console.log(response.data);
   return await response.data;
 }
+
+// 'https://api.themoviedb.org/3/trending/all/day?api_key=e5eae3e86944a38d68a9b48044e5baf0'
