901031b05d89215f3259c8ac5c92b023ab6698ce
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-04 20:56:05 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-export async function fetchMovies() {
-  const response = await axios.get(
-    'https://api.themoviedb.org/3/search/movie?api_key=e5eae3e86944a38d68a9b48044e5baf0&include_adult=false&language=en-US&page=1'
-  );
-  console.log(response.data);
-  return await response.data;
-}
+const API_KEY = 'e5eae3e86944a38d68a9b48044e5baf0';
 
-// 'https://api.themoviedb.org/3/trending/all/day?api_key=e5eae3e86944a38d68a9b48044e5baf0'
+export const fetchFavouriteMovies = async () => {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+  });
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/trending/all/day'
+  );
+  return await response.data;
+};
