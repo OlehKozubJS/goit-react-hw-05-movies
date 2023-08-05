61b39a1009820cd4a27fec4f14b95c31abb55d3f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-06 00:27:56 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -5,27 +5,26 @@ import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 export const MovieDetails = () => {
   const { movieId } = useParams();
   const location = useLocation();
-  const [movieDetails, setMovieDetails] = useState('');
+  const [movieDetails, setMovieDetails] = useState([]);
   const backLinkHref = location.state?.from ?? '/movies';
 
   useEffect(() => {
     const getMovieDetails = async () => {
       try {
         const result = await fetchMovieDetails(movieId);
-        setMovieDetails(result.title);
+        setMovieDetails(result);
         console.log(result);
       } catch {
         console.log('The fetch has failed');
       }
     };
     getMovieDetails();
-    console.log(movieDetails);
   }, []);
 
   return (
     <div>
       <Link to={backLinkHref}>Back to movies</Link>
-      <h2></h2>
+      <h2>{movieDetails.title}</h2>
       <p></p>
       <h3>Overview</h3>
       <p></p>
