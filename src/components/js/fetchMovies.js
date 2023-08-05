52a8789b5dfe7f475ad6a8aa9d5ebf355377cd2e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-05 23:06:28 +0300

    Update fetchMovies.js

--- src/components/js/fetchMovies.js
+++ src/components/js/fetchMovies.js
@@ -26,7 +26,7 @@ export const fetchMovieDetails = async id => {
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
   });
-  const response = await axios.get(URL + `movie/${id}?` + searchParams);
+  const response = await axios.get(URL + 'movie/' + id + '?' + searchParams);
   console.log(response.data);
   return await response.data;
 };
