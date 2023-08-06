04d6100f4491b2b98f8c2236ca39f5bcf5a5c4ec
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-06 16:05:35 +0300

    Update Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,8 +1,55 @@
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { fetchMovieDetails } from './js/fetchMovies';
+import { Suspense, useEffect, useState } from 'react';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 
 export const Cast = () => {
-  return <div></div>;
+  const { movieId } = useParams();
+  const location = useLocation();
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [score, setScore] = useState(0);
+  const [overview, setOverview] = useState('');
+  const [genres, setGenres] = useState([]);
+  const backLinkHref = location.state?.from ?? '/movies';
+
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      try {
+        const result = await fetchMovieDetails(movieId);
+        setTitle(result.title);
+        setImage(result.poster_path);
+        setOverview(result.overview);
+        setScore(result.vote_average);
+        setGenres(result.genres);
+        console.log(result);
+      } catch {
+        console.log('The fetch has failed');
+      }
+    };
+    getMovieDetails();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <img src={`https://image.tmdb.org/t/p/w500` + image} alt="" />
+        <Link to={backLinkHref}>Back to movies</Link>
+        <h2>{title}</h2>
+        <p>Use Score: {Math.floor(score * 10)}</p>
+        <h3>Overview</h3>
+        <p>{overview}</p>
+        <h4>Genres</h4>
+      </div>
+      <ul>
+        {genres.map(genre => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+      <Suspense fallback={<div>Loading subpage...</div>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Cast;
