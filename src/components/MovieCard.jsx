fea513c36f9d73018c04767366f54357580995a0
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-08 17:41:13 +0300

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -27,12 +27,14 @@ export const MovieCard = ({ movieId }) => {
   return (
     <div>
       <div>
-        <img
-          style={{ display: 'block' }}
-          width="100px"
-          src={`https://image.tmdb.org/t/p/w500` + image}
-          alt=""
-        />
+        {image && (
+          <img
+            style={{ display: 'block' }}
+            width="100px"
+            src={`https://image.tmdb.org/t/p/` + image}
+            alt=""
+          />
+        )}
         <h2>{title}</h2>
         <p>Use Score: {Math.floor(score * 10)}%</p>
         <h4>Genres</h4>
