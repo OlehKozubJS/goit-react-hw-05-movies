648c9e80b16b1eaaa350b6e2847bdd829f58ae52
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-08 17:40:27 +0300

    Update MovieDetails.jsx

--- src/components/pages/MovieDetails.jsx
+++ src/components/pages/MovieDetails.jsx
@@ -32,12 +32,14 @@ export const MovieDetails = () => {
     <div>
       <div>
         <Link to={backLinkHref}>Back to movies</Link>
-        <img
-          style={{ display: 'block' }}
-          width="100px"
-          src={`https://image.tmdb.org/t/p/w500` + image}
-          alt=""
-        />
+        {image && (
+          <img
+            style={{ display: 'block' }}
+            width="100px"
+            src={`https://image.tmdb.org/t/p/` + image}
+            alt=""
+          />
+        )}
         <h2>{title}</h2>
         <p>Use Score: {Math.floor(score * 10)}%</p>
         <h3>Overview</h3>
