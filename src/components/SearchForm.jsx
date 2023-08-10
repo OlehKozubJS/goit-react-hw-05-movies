b62a030a0c7ed2c578331ed7c6b57d7fc9150da8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-10 18:16:13 +0300

    Create SearchForm.jsx

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,55 @@
+import { fetchMovieByName } from '../js/fetchMovies';
+import { useEffect, useState } from 'react';
+import MovieCard from 'components/MovieCard';
+import MoviesCSS from '../css/Movies.module.css';
+import { useSearchParams } from 'react-router-dom';
+
+export const Movies = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const searchText = event.currentTarget.elements.movieNameInput.value;
+    setSearchQuery(searchText);
+    event.currentTarget.reset();
+  };
+
+  useEffect(() => {
+    const getMovieByName = async () => {
+      try {
+        let result = await fetchMovieByName(searchQuery);
+        result = await result.results;
+        setMovies(result);
+      } catch {
+        console.log('The fetch has failed');
+      }
+    };
+    getMovieByName();
+  }, [searchQuery]);
+
+  return (
+    <div>
+      <form className={MoviesCSS.MovieSearch} onSubmit={handleSubmit}>
+        <input
+          className={MoviesCSS.MovieSearchInput}
+          name="movieNameInput"
+          type="text"
+        />
+        <button className={MoviesCSS.MovieSearchSubmit} type="submit">
+          Search
+        </button>
+      </form>
+      {searchQuery && (
+        <ul className={MoviesCSS.MovieCards}>
+          {movies.map(({ id }) => (
+            <MovieCard key={id} movieId={id} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Movies;
