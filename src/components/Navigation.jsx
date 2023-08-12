871b0270c8f3bc71a4d0d87d03e1c198451a1a18
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-13 00:07:02 +0300

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,11 +2,15 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import MovieDetailsCSS from './css/MovieDetails.module.css';
 
-const StyledLink = styled(NavLink)`
+const sls = `
   &.active {
     background-color: rgb(75, 200, 75);
     color: rgb(245, 250, 245);
   }
+`;
+
+const StyledLink = styled(NavLink)`
+  ${sls}
 `;
 
 export const Navigation = ({ links }) => {
