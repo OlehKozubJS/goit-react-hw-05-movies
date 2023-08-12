103f00d7b61056dd1057f650e31f3dcca179022d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-12 21:09:15 +0300

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,8 +13,7 @@ export const MovieCard = ({ movie }) => {
     const getGenresByIds = async () => {
       try {
         let result = await fetchMovieGenres(movie.genre_ids);
-        result = await result.genres;
-        setGenres(result);
+        //setGenres(result);
       } catch {
         console.log('The fetch has failed');
       }
