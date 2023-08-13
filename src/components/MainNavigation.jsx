78dcc77971c225d54b89a25dd6b8516470835d4f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-13 13:50:27 +0300

    Create MainNavigation.jsx

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import MovieDetailsCSS from './css/MovieDetails.module.css';
+
+const StyledLink = styled(NavLink)`
+  &.active {
+    background-color: rgb(75, 200, 75);
+    color: rgb(245, 250, 245);
+  }
+`;
+
+export const MainNavigation = ({ links }) => {
+  return (
+    <nav className={MovieDetailsCSS.MovieDetailsInnerNavigation}>
+      {links.map((link, linkIndex) => (
+        <StyledLink
+          key={linkIndex}
+          className={MovieDetailsCSS.MovieDetailsInnerNavigationLink}
+          to={link.to}
+        >
+          {link.title}
+        </StyledLink>
+      ))}
+    </nav>
+  );
+};
+
+export default Navigation;
