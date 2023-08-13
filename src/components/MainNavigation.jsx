0c6c4e03910cef65ed1f1e059aa153dcfae0456e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-13 14:24:14 +0300

    Update MainNavigation.jsx

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -4,18 +4,18 @@ import MovieDetailsCSS from './css/MovieDetails.module.css';
 
 const StyledLink = styled(NavLink)`
   &.active {
-    background-color: rgb(75, 200, 75);
-    color: rgb(245, 250, 245);
+    background-color: rgb(125, 125, 250);
+    color: rgb(245, 245, 250);
   }
 `;
 
 export const MainNavigation = ({ links }) => {
   return (
-    <nav className={MovieDetailsCSS.MovieDetailsInnerNavigation}>
+    <nav className={MovieDetailsCSS.Navigation}>
       {links.map((link, linkIndex) => (
         <StyledLink
           key={linkIndex}
-          className={MovieDetailsCSS.MovieDetailsInnerNavigationLink}
+          className={MovieDetailsCSS.NavigationLink}
           to={link.to}
         >
           {link.title}
