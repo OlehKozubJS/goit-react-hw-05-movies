6fe76ea3bd38b5dd1c7ab73935ea6d3ccc72a91a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-13 14:21:43 +0300

    Update MovieDetails.jsx

--- src/components/pages/MovieDetails.jsx
+++ src/components/pages/MovieDetails.jsx
@@ -3,15 +3,7 @@ import { Suspense, useEffect, useState } from 'react';
 import { NavLink, Outlet, useLocation, useParams } from 'react-router-dom';
 import MovieDetailsCSS from '../css/MovieDetails.module.css';
 import AppCSS from '../css/App.module.css';
-import styled from 'styled-components';
 import Navigation from 'components/Navigation';
-
-const StyledLink = styled(NavLink)`
-  &.active {
-    background-color: rgb(75, 200, 75);
-    color: rgb(245, 250, 245);
-  }
-`;
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
